faddb72e50cd378a3eca28d91c00e8d55b8cf472
MEHDIMYADI <MEHDIMYADI@Home>
2024-09-08 18:01:23 +0330

    Fix Bugs

--- MultiDisplayTool/Properties/AssemblyInfo.cs
+++ MultiDisplayTool/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("MEHDIMYADI")]
 [assembly: AssemblyProduct("MultiDisplayTool")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyCopyright("MEHDIMYADI Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
